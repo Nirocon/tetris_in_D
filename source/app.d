daa03b14650ee6ae44868bd91a61d992ab3b4277
Nirocon <166694799+Nirocon@users.noreply.github.com>
2025-08-03 17:12:49 +0900

    fix: 関数の依存性を少なくするため、グローバル変数や引数などを修正、また繰り返し構文の可読性向上のため、構文の調整

--- source/app.d
+++ source/app.d
@@ -62,150 +62,43 @@ enum BOARD_WIDTH = 10;
 enum BOARD_HEIGHT = 20;
 enum TILE_SIZE = 30;
 
-int[BOARD_WIDTH][BOARD_HEIGHT] board; // Note the order of dimensions: [height][width]
-int[4][4] currentBlock;
-int blockX = 3, blockY = 0;
-int currentBlockIndex;
-
-void spawnBlock() {
+/// 
+/// Params:
+///   currentBlock = int[4][4]
+///   currentBlockIndex = int
+///   blockX = int
+///   blockY = int
+void spawnBlock(
+                    ref int[4][4] currentBlock,
+                    ref int currentBlockIndex,
+                    ref int blockX,
+                    ref int blockY
+                ) {
     currentBlockIndex = uniform(0, 7);
     currentBlock = tetrominoShapes[currentBlockIndex];
     blockX = 3;
     blockY = 0;
 }
 
-bool checkCollision() {
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            int drawX = blockX + x;
-            int drawY = blockY + y;
-            if (currentBlock[y][x] == 1) {
-                // Boundary check to ensure the block stays inside the grid
-                if (drawX < 0 || drawX >= BOARD_WIDTH || drawY >= BOARD_HEIGHT || board[drawY][drawX] != 0) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
-void fixBlock() {
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            int drawX = blockX + x;
-            int drawY = blockY + y;
-            if (currentBlock[y][x] == 1) {
-                if (drawX >= 0 && drawX < BOARD_WIDTH && drawY >= 0 && drawY < BOARD_HEIGHT) {
-                    board[drawY][drawX] = 1;
-                }
-            }
-        }
-    }
-    clearFullLines();  // ラインをクリア
-}
-
-int getLeftmostX(ref int[4][4] block) {
-    int minX = 4;
-    foreach (y; 0 .. 4) {
-        foreach (x; 0 .. 4) {
-            if (block[y][x] == 1 && x < minX) {
-                minX = x;
-            }
-        }
-    }
-    return minX;
-}
-
-int getRightmostX(ref int[4][4] block) {
-    int maxX = -1;
-    foreach (y; 0 .. 4) {
-        foreach (x; 0 .. 4) {
-            if (block[y][x] == 1 && x > maxX) {
-                maxX = x;
-            }
-        }
-    }
-    return maxX;
-}
-
-void adjustBlockPosition() {
-    int left = getLeftmostX(currentBlock);
-    int right = getRightmostX(currentBlock);
-
-    // 左端が画面外 or 衝突してたら右にずらす
-    while (blockX + left < 0 || checkCollision(currentBlock)) {
-        blockX++;
-        if (checkCollision(currentBlock)) {
-            blockX--; // ずらしても解決しなかったら戻してbreak
-            break;
-        }
-        left = getLeftmostX(currentBlock);
-    }
-
-    // 右端が画面外 or 衝突してたら左にずらす
-    while (blockX + right >= BOARD_WIDTH || checkCollision(currentBlock)) {
-        blockX--;
-        if (checkCollision(currentBlock)) {
-            blockX++; // ずらしても解決しなかったら戻してbreak
-            break;
-        }
-        right = getRightmostX(currentBlock);
-    }
-}
-
-void drawBlock() {
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            int drawX = blockX + x;
-            int drawY = blockY + y;
-            if (currentBlock[y][x] == 1 && drawX >= 0 && drawX < BOARD_WIDTH && drawY >= 0 && drawY < BOARD_HEIGHT) {
-                DrawRectangle(drawX * TILE_SIZE, drawY * TILE_SIZE, TILE_SIZE - 1, TILE_SIZE - 1, Colors.MAROON);
-            }
-        }
-    }
-}
-
-void rotateBlock() {
-    int[4][4] rotatedBlock;
-    foreach (y; 0 .. 4)
-        foreach (x; 0 .. 4)
-            rotatedBlock[x][3 - y] = currentBlock[y][x];
-
-    if (!checkCollision(rotatedBlock)) {
-        currentBlock = rotatedBlock;
-    } else {
-		tryWallKick(rotatedBlock);
-	}
-}
-
-void tryWallKick(int[4][4] rotatedBlock) {
-    // 試しに左に1マスずらしてチェック
-    blockX -= 1;
-    if (!checkCollision(rotatedBlock)) {
-        currentBlock = rotatedBlock;
-        return;
-    }
-
-    // 左にずらしてダメだったら元に戻して、右に1マスずらす
-    blockX += 2;
-    if (!checkCollision(rotatedBlock)) {
-        currentBlock = rotatedBlock;
-        return;
-    }
-
-    // それでもダメなら元の位置に戻して何もしない
-    blockX -= 1;
-}
-
-
-bool checkCollision(int[4][4] block) {
+/// 
+/// Params:
+///   board = int[BOARD_WIDTH][BOARD_HEIGHT]
+///   currentBlock = int[4][4]
+///   blockX = int
+///   blockY = int
+/// Returns: bool
+bool checkCollision(
+                        ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                        ref int[4][4] currentBlock, 
+                        ref int blockX,
+                        ref int blockY
+                    ) {
     // 回転したブロックで衝突をチェック
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
+    foreach (y; 0 .. 4) {
+        foreach (x; 0 .. 4) {
             int drawX = blockX + x;
             int drawY = blockY + y;
-            if (block[y][x] == 1) {
+            if (currentBlock[y][x] == 1) {
                 // ボードの範囲外か、すでにブロックがある場合は衝突
                 if (drawX < 0 || drawX >= BOARD_WIDTH || drawY >= BOARD_HEIGHT || board[drawY][drawX] != 0) {
                     return true;
@@ -216,21 +109,211 @@ bool checkCollision(int[4][4] block) {
     return false;
 }
 
-void hardDrop() {
-    while (!checkCollision()) {
+void fixBlock(
+                ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                ref int[4][4] currentBlock,
+                ref int blockX,
+                ref int blockY
+            ) {
+    foreach (y; 0 .. 4) {
+        foreach (x; 0 .. 4) {
+            int drawX = blockX + x;
+            int drawY = blockY + y;
+            if (currentBlock[y][x] == 1) {
+                if (drawX >= 0 && drawX < BOARD_WIDTH && drawY >= 0 && drawY < BOARD_HEIGHT) {
+                    board[drawY][drawX] = 1;
+                }
+            }
+        }
+    }
+    clearFullLines(board);  // ラインをクリア
+}
+
+int getLeftmostX(ref int[4][4] currentBlock) {
+    int minX = 4;
+    foreach (y; 0 .. 4) {
+        foreach (x; 0 .. 4) {
+            if (currentBlock[y][x] == 1 && x < minX) {
+                minX = x;
+            }
+        }
+    }
+    return minX;
+}
+
+int getRightmostX(ref int[4][4] currentBlock) {
+    int maxX = -1;
+    foreach (y; 0 .. 4) {
+        foreach (x; 0 .. 4) {
+            if (currentBlock[y][x] == 1 && x > maxX) {
+                maxX = x;
+            }
+        }
+    }
+    return maxX;
+}
+
+/// 
+/// Params:
+///   board = int[BOARD_WIDTH][BOARD_HEIGHT]
+///   currentBlock = int[4][4]
+///   blockX = int
+///   blockY = int
+void adjustBlockPosition(
+                            ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                            ref int[4][4] currentBlock,
+                            ref int blockX,
+                            ref int blockY
+                        ) {
+    int left = getLeftmostX(currentBlock);
+    int right = getRightmostX(currentBlock);
+
+    // 左端が画面外 or 衝突してたら右にずらす
+    while (blockX + left < 0 || checkCollision(board, currentBlock, blockX, blockY)) {
+        blockX++;
+        if (checkCollision(board, currentBlock, blockX, blockY)) {
+            blockX--; // ずらしても解決しなかったら戻してbreak
+            break;
+        }
+        left = getLeftmostX(currentBlock);
+    }
+
+    // 右端が画面外 or 衝突してたら左にずらす
+    while (blockX + right >= BOARD_WIDTH || checkCollision(board, currentBlock, blockX, blockY)) {
+        blockX--;
+        if (checkCollision(board, currentBlock, blockX, blockY)) {
+            blockX++; // ずらしても解決しなかったら戻してbreak
+            break;
+        }
+        right = getRightmostX(currentBlock);
+    }
+}
+
+/// 
+/// Params:
+///   currentBlock = int[4][4]
+///   blockX = int
+///   blockY = int
+void drawBlock  (
+                    ref int[4][4] currentBlock,
+                    ref int blockX,
+                    ref int blockY
+                ) {
+    foreach (y; 0 .. 4) {
+        foreach (x; 0 .. 4) {
+            int drawX = blockX + x;
+            int drawY = blockY + y;
+            if (currentBlock[y][x] == 1 && drawX >= 0 && drawX < BOARD_WIDTH && drawY >= 0 && drawY < BOARD_HEIGHT) {
+                DrawRectangle(drawX * TILE_SIZE, drawY * TILE_SIZE, TILE_SIZE - 1, TILE_SIZE - 1, Colors.MAROON);
+            }
+        }
+    }
+}
+
+/// 
+/// Params:
+///   board = int[BOARD_WIDTH][BOARD_HEIGHT]
+///   currentBlock = int[4][4]
+///   blockX = int
+///   blockY = int
+void rotateBlock(
+                    ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                    ref int[4][4] currentBlock,
+                    ref int blockX,
+                    ref int blockY
+                ) {
+    int[4][4] rotatedBlock;
+    foreach (y; 0 .. 4)
+        foreach (x; 0 .. 4)
+            rotatedBlock[x][3 - y] = currentBlock[y][x];
+
+    if (!checkCollision(board, rotatedBlock, blockX, blockY)) {
+        currentBlock = rotatedBlock;
+    } else {
+		tryWallKick(board, currentBlock, rotatedBlock, blockX, blockY);
+	}
+}
+
+/// 
+/// Params:
+///   board = int[BOARD_WIDTH][BOARD_HEIGHT]
+///   currentBlock = int[4][4]
+///   rotatedBlock = int[4][4]
+///   blockX = int
+///   blockY = int
+void tryWallKick(
+                    ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                    ref int[4][4] currentBlock,
+                    ref int[4][4] rotatedBlock,
+                    ref int blockX,
+                    ref int blockY
+                ) {
+    // 試しに左に1マスずらしてチェック
+    blockX -= 1;
+    if (!checkCollision(board, rotatedBlock, blockX, blockY)) {
+        currentBlock = rotatedBlock;
+        return;
+    }
+
+    // 左にずらしてダメだったら元に戻して、右に1マスずらす
+    blockX += 2;
+    if (!checkCollision(board, rotatedBlock, blockX, blockY)) {
+        currentBlock = rotatedBlock;
+        return;
+    }
+
+    // それでもダメなら元の位置に戻して何もしない
+    blockX -= 1;
+}
+
+
+// bool checkCollision(int[4][4] currentBlock) {
+//     // 回転したブロックで衝突をチェック
+//     foreach (y; 0 .. 4) {
+//         foreach (x; 0 .. 4) {
+//             int drawX = blockX + x;
+//             int drawY = blockY + y;
+//             if (currentBlock[y][x] == 1) {
+//                 // ボードの範囲外か、すでにブロックがある場合は衝突
+//                 if (drawX < 0 || drawX >= BOARD_WIDTH || drawY >= BOARD_HEIGHT || board[drawY][drawX] != 0) {
+//                     return true;
+//                 }
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+/// 
+/// Params:
+///   board = int[BOARD_WIDTH][BOARD_HEIGHT]
+///   currentBlock = int[4][4]
+///   currentBlockIndex = int
+///   blockX = int
+///   blockY = int
+void hardDrop(
+                ref int[BOARD_WIDTH][BOARD_HEIGHT] board,
+                ref int[4][4] currentBlock,
+                ref int currentBlockIndex,
+                ref int blockX,
+                ref int blockY
+            ) {
+    while (!checkCollision(board, currentBlock, blockX, blockY)) {
         blockY++;
     }
     blockY--;           // 衝突したから1マス戻す
-    fixBlock();         // 盤面に固定
-    spawnBlock();       // 次のブロック
+    fixBlock(board, currentBlock, blockX, blockY);         // 盤面に固定
+    spawnBlock(currentBlock, currentBlockIndex, blockX, blockY);       // 次のブロック
 }
 
-void clearFullLines() {
+void clearFullLines(
+                    ref int[BOARD_WIDTH][BOARD_HEIGHT] board
+) {
     // ラインが揃っているかチェックして、揃っている場合は削除
     for (int y = BOARD_HEIGHT - 1; y >= 0; y--) {
         bool isFullLine = true;
         // 各行をチェック
-        for (int x = 0; x < BOARD_WIDTH; x++) {
+        foreach (x; 0 .. BOARD_WIDTH) {
             if (board[y][x] == 0) {
                 isFullLine = false;
                 break;
@@ -241,12 +324,12 @@ void clearFullLines() {
         if (isFullLine) {
             // 行を下にシフト
             for (int shiftY = y; shiftY > 0; shiftY--) {
-                for (int x = 0; x < BOARD_WIDTH; x++) {
+                foreach (x; 0 .. BOARD_WIDTH) {
                     board[shiftY][x] = board[shiftY - 1][x];
                 }
             }
             // 最上行をクリア
-            for (int x = 0; x < BOARD_WIDTH; x++) {
+            foreach (x; 0 .. BOARD_WIDTH) {
                 board[0][x] = 0;
             }
             y++;  // 1行削除したので、再度同じyをチェックする
@@ -255,10 +338,15 @@ void clearFullLines() {
 }
 
 void main() {
+    int[BOARD_WIDTH][BOARD_HEIGHT] board; // Note the order of dimensions: [height][width]
+    int[4][4] currentBlock;
+    int blockX = 3, blockY = 0;
+    int currentBlockIndex;
+
     InitWindow(BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE, "D テトリス");
 
     SetTargetFPS(60);
-    spawnBlock();
+    spawnBlock(currentBlock, currentBlockIndex, blockX, blockY);
 
     MonoTime lastDrop = MonoTime.currTime;
     Duration dropInterval = 500.msecs;
@@ -267,18 +355,18 @@ void main() {
         // 入力処理
         if (IsKeyPressed(KeyboardKey.KEY_LEFT))  	blockX--;
         if (IsKeyPressed(KeyboardKey.KEY_RIGHT)) 	blockX++;
-        if (IsKeyPressed(KeyboardKey.KEY_DOWN))  	hardDrop();
-		if (IsKeyPressed(KeyboardKey.KEY_Z)) 		rotateBlock();  // 回転処理
+        if (IsKeyPressed(KeyboardKey.KEY_DOWN))  	hardDrop(board, currentBlock, currentBlockIndex, blockX, blockY);
+		if (IsKeyPressed(KeyboardKey.KEY_Z)) 		rotateBlock(board, currentBlock, blockX, blockY);  // 回転処理
 
-        adjustBlockPosition();  // テトミノの位置補正
+        adjustBlockPosition(board, currentBlock, blockX, blockY);  // テトミノの位置補正
 
         // 自動で下に落ちる処理
         if (MonoTime.currTime - lastDrop > dropInterval) {
             blockY++;
-            if (checkCollision()) {
-                blockY--; // Revert the block's position
-                fixBlock(); // Fix the block to the board
-                spawnBlock(); // Spawn a new block
+            if (checkCollision(board, currentBlock, blockX, blockY)) {
+                blockY--; // Revert the currentBlock's position
+                fixBlock(board, currentBlock, blockX, blockY); // Fix the currentBlock to the board
+                spawnBlock(currentBlock, currentBlockIndex, blockX, blockY); // Spawn a new currentBlock
             }
             lastDrop = MonoTime.currTime;
         }
@@ -287,8 +375,8 @@ void main() {
         ClearBackground(Colors.RAYWHITE);
 
         // 盤面の描画
-        for (int y = 0; y < BOARD_HEIGHT; y++) {
-            for (int x = 0; x < BOARD_WIDTH; x++) {
+        foreach (y; 0 .. BOARD_HEIGHT) {
+            foreach (x; 0 .. BOARD_WIDTH) {
                 if (board[y][x] != 0) {
                     DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE - 1, TILE_SIZE - 1, Colors.DARKGRAY);
                 }
@@ -296,7 +384,7 @@ void main() {
         }
 
         // ブロックの描画
-        drawBlock();
+        drawBlock(currentBlock, blockX, blockY);
 
         EndDrawing();
     }
